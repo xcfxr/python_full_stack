4eeafa14cd059438d75ca1ed99a0949d1d16418d
xucee <xucee@qq.com>
2021-12-07 21:52:07 +0800

    2021.12-7

--- try.py
+++ try.py
@@ -1,8 +1,1 @@
-from psd_tools import PSDImage
-img = PSDImage.open('./2228910733574.PSD')
-for layer in img:
-    layer.topil().save(f'{layer.name}.png')
-    print(layer.top)
-    print(layer.bottom)
-    print(layer.left)
-    print(layer.right)
+print(2 is 2 is 2)
