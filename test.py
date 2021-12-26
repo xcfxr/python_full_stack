a9aab456a76e1cdac2ce646193cdb4fd282f52c2
xucee <xucee@qq.com>
2021-12-26 23:48:15 +0800

    2021-12-26

--- test.py
+++ test.py
@@ -3,17 +3,15 @@ import os
 
 def copy_file(s, t):
     for each in os.listdir(s):
-        if os.path.isfile(os.path.join(s, each)):
-            with open(os.path.join(s, each), 'rb') as f1, \
-                    open(os.path.join(t, each), 'wb') as f2:
+        if os.path.isfile(os.path.join(s, each, '今日内容.md')):
+            with open(os.path.join(s, each, '今日内容.md'), 'rb') as f1, \
+                    open(os.path.join(t, '今日内容.md'), 'wb') as f2:
                 for line in f1:
                     f2.write(line)
-        else:
-            os.makedirs(os.path.join(t, each), exist_ok=True)
-            copy_file(os.path.join(s, each), os.path.join(t, each))
 
-for day in range(42, 43):
+
+for day in range(44, 49):
     os.makedirs('day%d'%day, exist_ok=True)
-    copy_file('D:/BaiduNetdiskDownload/04阶段高并发 线程池进程协程通信协议/day%d/代码/day%d'%(day, day), 'day%d'%day)
+    copy_file(r'D:\BaiduNetdiskDownload\05阶段数据库 多表查询 增删改查', 'day%d'%day)
 
 
